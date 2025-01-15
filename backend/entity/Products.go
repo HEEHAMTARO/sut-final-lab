1890ce2f6a50feacd7f09c68e6c2f81123ae499b
HEEHAMTARO <jaonai0954400179@gmail.com>
2025-01-15 11:35:36 +0700

    b6511326_negative_price_test - close #4

--- backend/entity/Products.go
+++ backend/entity/Products.go
@@ -7,6 +7,6 @@ import (
 type Products struct {
 	gorm.Model
 	Name string `valid:"required~Name is required"`
-	Price float64 `valid:"required~Price is required"`
+	Price float64 `valid:"required~Price is required, range(1|1000)"`
 	SKU string `valid:"required~SKU is required"`
 }
